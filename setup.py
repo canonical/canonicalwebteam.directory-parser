9a38b2bbb32b602da8f2b6da00c0d150d06bf962
Britney Wang <britney.wang@canonical.com>
2025-04-17 16:50:20 +0800

    chore: Bump package ver

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.directory-parser",
-    version="1.2.7",
+    version="1.2.8",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.directory-parser",
